08327c3aa3feb650172be127ce3372cfeabed644
Arundeepak.M <Arundeepak.M@localhost>
2022-02-24 11:34:38 +0530

    Day 1 Leet Code Two Sum

--- src/com/leetcode/Day1_TwoSum.java
+++ src/com/leetcode/Day1_TwoSum.java
@@ -3,7 +3,29 @@ package com.leetcode;
 public class Day1_TwoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arr[] = { 2, 2, 3,4 };
+
+		int target = 7;
+
+		int[] result = findTwoSum(arr, target);
+
+		for(int a: result) {
+			System.out.println(a);
+		}
+
+	}
+
+	public static int[] findTwoSum(int[] arr, int target) {
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+
+		return new int[] {};
 
 	}
 
